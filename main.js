6ae49b8f2c960e8076b86f3d9e525c37cd71cd03
Salmanul Farzy <salmanulfarzy@gmail.com>
2017-11-25 16:50:58 +0530

    Load webpage into BrowserWindow

---
+++ main.js
@@ -0,0 +1,35 @@
+const electron = require('electron')
+const app = electron.app
+
+const BrowserWindow = electron.BrowserWindow
+
+const path = require('path')
+const url = require('url')
+
+let mainWindow
+
+function createWindow () {
+  mainWindow = new BrowserWindow({
+    title: app.getName(),
+  })
+
+  mainWindow.loadURL('https://www.saavn.com/')
+
+  mainWindow.on('closed', function () {
+    mainWindow = null
+  })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', function () {
+  if (mainWindow === null) {
+    createWindow()
+  }
+})
